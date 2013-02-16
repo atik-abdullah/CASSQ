51e93084e3234b15e0262e271b30dabed809f432
Atii <neo_ati@yahoo.com>
2013-02-17 00:23:48 +0200

    SeventhViewController implemented

--- CASSQ/SeventhViewController.h
+++ CASSQ/SeventhViewController.h
@@ -8,9 +8,14 @@
 
 #import <UIKit/UIKit.h>
 
-@interface SeventhViewController : UIViewController
+@interface SeventhViewController : UIViewController < UIImagePickerControllerDelegate , UINavigationControllerDelegate >
 
 // Public properties
 @property (nonatomic, copy) NSDictionary *postSelection;
 @property (nonatomic, weak) IBOutlet UILabel *question;
+@property (nonatomic, weak) IBOutlet UIImageView *myImageView; // Needed to set photos , if the user answered previously
+@property (nonatomic, weak) IBOutlet UIButton *saveButton; // Needed to change the text on it inform user "Answered"
+
+- (IBAction)takePicture:(id)sender;
+- (IBAction)saveAnswer:(id)sender;
 @end
--- CASSQ/SeventhViewController.m
+++ CASSQ/SeventhViewController.m
@@ -11,7 +11,7 @@
 
 @interface SeventhViewController ()
 @property (nonatomic, strong) Item *item;
-
+@property  (nonatomic , strong) UIImage *imageToSaveInCoreData; // The media that needs to be saved in database.
 @end
 
 @implementation SeventhViewController
@@ -21,6 +21,100 @@
     [super viewDidLoad];
     self.item = [self.postSelection objectForKey:@"selectedItem"];
     self.question.text = self.item.question;
+    
+    // If the user has answered it previously
+    if (self.item.photo)
+    {
+        [self.myImageView setImage:self.item.photo];
+    }
 }
 
+- (IBAction)takePicture:(id)sender
+{
+    UIImagePickerController *imagePicker =[[UIImagePickerController alloc] init];
+    
+    // If our device has a camera, we want to take a picture, otherwise, we just pick from photo library
+    if ([UIImagePickerController
+         isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera])
+    {
+        [imagePicker setSourceType:UIImagePickerControllerSourceTypeCamera];
+    }
+    else
+    {
+        [imagePicker setSourceType:UIImagePickerControllerSourceTypePhotoLibrary];
+    }
+    
+    // This line of code will generate a warning right now, ignore it
+    [imagePicker setDelegate:self];
+    
+    // Place image picker on the screen
+    [self presentViewController:imagePicker animated:YES completion:nil];
+}
+
+- (void)imagePickerController:(UIImagePickerController *)picker
+didFinishPickingMediaWithInfo:(NSDictionary *)info
+{
+    // Get picked image from info dictionary
+    UIImage *image = [info objectForKey:UIImagePickerControllerOriginalImage];
+    
+    // Put that image onto the screen in our image view
+    UIImageWriteToSavedPhotosAlbum (image, nil, nil , nil);
+    [self.myImageView setImage:image];
+    self.imageToSaveInCoreData = image;
+    
+    // Take image picker off the screen - you must call this dismiss method
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+
+
+
+- (IBAction)saveAnswer:(id)sender
+{
+    // Create name of the audio file which should have "photo_2013.02.16._17_11.20.jpg" format.
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    NSMutableString *nameString  = [[NSMutableString alloc] init];
+
+    [nameString appendString:@"photo_"];
+    
+    [dateFormatter setDateFormat:@"yyyy"];
+    [nameString appendString:[dateFormatter stringFromDate:[NSDate date]]];
+    [nameString appendString:@"."];
+    
+    [dateFormatter setDateFormat:@"MM"];
+    [nameString appendString:[dateFormatter stringFromDate:[NSDate date]]];
+    [nameString appendString:@"."];
+    
+    [dateFormatter setDateFormat:@"dd"];
+    [nameString appendString:[dateFormatter stringFromDate:[NSDate date]]];
+    [nameString appendString:@"."];
+    [nameString appendString:@"_"];
+    
+    [dateFormatter setDateFormat:@"HH"];
+    [nameString appendString:[dateFormatter stringFromDate:[NSDate date]]];
+    [nameString appendString:@"_"];
+    
+    [dateFormatter setDateFormat:@"mm"];
+    [nameString appendString:[dateFormatter stringFromDate:[NSDate date]]];
+    [nameString appendString:@"."];
+    
+    [dateFormatter setDateFormat:@"ss"];
+    [nameString appendString:[dateFormatter stringFromDate:[NSDate date]]];
+    [nameString appendString:@".jpg"];
+    
+    // Save the name of the media to database
+    self.item.answerText = nameString;
+    
+    // Save the media to database
+    self.item.photo = self.imageToSaveInCoreData;
+    
+    // Mark as answered
+    self.item.answered = [NSNumber numberWithBool:YES];
+    
+    // Save context
+    [[self.item managedObjectContext] save:nil];
+    
+    // Change button text to inform user that he has answered successfully
+    [self.saveButton setTitle:@"Answered" forState:UIControlStateNormal] ;
+}
 @end
